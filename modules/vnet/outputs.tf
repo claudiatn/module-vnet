65d17dbb7812a70838fa87ec72a35af0fb072501
claudiatn <cntenorio@stemdo.io>
2024-10-15 14:43:24 +0200

    Update outputs.tf

--- modules/vnet/outputs.tf
+++ modules/vnet/outputs.tf
@@ -1,34 +1,30 @@
 output "resource_group_name" {
   description = "Nombre del Resource Group usado en el despliegue"
-  value       = var.existent_resource_group_name
+  value       = azurerm_virtual_network.vnet.resource_group_name
 }
 
 output "vnet_name" {
   description = "Nombre de la Virtual Network"
-  value       = var.vnet_name
+  value       = azurerm_virtual_network.vnet.name
 }
 
 output "vnet_address_space" {
   description = "Espacio de direcciones asignado a la VNet"
-  value       = var.vnet_address_space
+  value       = azurerm_virtual_network.vnet.address_space
 }
 
 output "location" {
   description = "Ubicación donde se despliega la VNet"
-  value       = var.location
+  value       = azurerm_virtual_network.vnet.location
 }
 
 output "owner_tag" {
   description = "Propietario de la VNet"
-  value       = var.owner_tag
+  value       = azurerm_virtual_network.vnet.tags.owner
 }
 
 output "environment_tag" {
   description = "Entorno de la VNet"
-  value       = var.environment_tag
+  value       = azurerm_virtual_network.vnet.tags.environment
 }
 
-output "vnet_tags" {
-  description = "Tags adicionales aplicados a la VNet"
-  value       = var.vnet_tags
-}
